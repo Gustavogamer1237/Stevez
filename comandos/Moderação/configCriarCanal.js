53cffd56f2caad8c1a86b082c96dde3d5ecf26ef
oByu <agazim577@gmail.com>
2020-11-16 18:09:46 -0200

    Novo comando "configCriarCanal", serve para criação de canais personalziados.

---
+++ comandos/Moderação/configCriarCanal.js
@@ -0,0 +1,18 @@
+ module.exports = {
+  config: {
+    nome: 'configCriarCanal',                                                   
+    aliases: ['criarcanal', 'canal', 'channel'],                               
+    descricao: 'Criações de canais.',     
+    utilizacao: '',                                               
+    cooldown: 3                                                   
+  },
+
+
+run: async (client, message, args) => {
+	if(!message.member.hasPermission('MANAGE_CHANNELS')) return
+		if(!args[0]) return message channel.send("🔗 **| Insira o nome do canal.**");
+
+	message.guild.channels.create(args.slice(0).join(" "), {type: 'text'}) // 🔗 Text or voice
+	message.channel.send("🎉 **| Criado com sucesso!**")
+}
+}
