8abab63c20456a584a76d1e35edb6598d906baa7
oByu <agazim577@gmail.com>
2020-11-16 03:24:15 -0200

    :memo: | overwritePermissions
    Signed-off-by: oByu <agazim577@gmail.com>

--- comandos/Moderação/modUnlock.js
+++ comandos/Moderação/modUnlock.js
@@ -1,30 +1,26 @@
 module.exports = {
   config: {
     nome: 'modUnlock',                                                   
-    aliases: ['unlock', 'ulock', 'unchat'],                               
+    aliases: ['unlock', 'ulc', 'ulchat'],                               
     descricao: 'Comando que desbloqueia o chat.',     
     utilizacao: '',                                               
     cooldown: 3                                                   
   },
 
   run: async (client, message, args) => {
-     let canal = message.mentions.channels.first()
-   if(!canal) {
-      return message.channel.send("Mencione o canal.")
-    }
+   let everyone = message.guild.roles.everyone;
   const emoji = message.guild.emojis.cache.find(e => e.name === "config")
   const emoji2 = message.guild.emojis.cache.find(e => e.name === "concluido")
   if (!message.member.hasPermission(`MANAGE_CHANNELS`)) return 
 
     // Realiza as funções depois do comando.
-   canal.send(`<:${emoji.name}:${emoji.id}>** | Canal desbloqueado por ${message.author}**`)
+   message.channel.send(`<:${emoji.name}:${emoji.id}>** | Canal desbloqueado por ${message.author}**`)
    message.react(`<:${emoji2.name}:${emoji2.id}>`)
-   canal.updateOverwrite([
+   channel.overwritePermissions(everyone,[
   {
-     id: message.guild.id,
      true: ['SEND_MESSAGES'],
-     deny: ['ADD_REACTIONS', 'MENTION_EVERYONE']
   },
 ]);
   }
 }
+
